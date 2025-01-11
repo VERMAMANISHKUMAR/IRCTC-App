fa5447f702d481cad753660b9f804e750cf9a53e
Manish Kumar Verma <157243115+VERMAMANISHKUMAR@users.noreply.github.com>
2025-01-11 22:53:58 +0530

    this is first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Hello this is a react app</h1>
+    </>
+  )
+}
+
+export default App
